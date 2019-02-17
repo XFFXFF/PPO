89800e527294e3ecd9b95aee98ef1a8005084558
xff <sailbafan@gmail.com>
2019-02-17 16:46:14 +0800

    launch!!!

---
+++ ppo/model.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+from tensorflow import layers
+from tensorflow.distributions import Categorical
+
+
+class ActorCriticModel(object):
+
+    def __init__(self, obs, act_space):
+        with tf.variable_scope('pi'):
+            x = self._cnn(obs)
+            logits = layers.dense(x, units=act_space.n)
+            self.dist = Categorical(logits=logits)
+        with tf.variable_scope('old_pi'):
+            x = self._cnn(obs)
+            logits = layers.dense(x, units=act_space.n)
+            self.old_dist = Categorical(logits=logits)
+        with tf.variable_scope('v'):
+            x = self._cnn(obs)
+            self.val = tf.squeeze(layers.dense(x, units=1))
+
+    def _cnn(self, x):
+        # x = layers.conv2d(x, filters=32, kernel_size=8, strides=(4, 4), activation=tf.nn.tanh)
+        # x = layers.conv2d(x, filters=64, kernel_size=4, strides=(2, 2), activation=tf.nn.tanh)
+        # x = layers.conv2d(x, filters=64, kernel_size=3, strides=(1, 1), activation=tf.nn.tanh)
+        # x = layers.flatten(x)
+        # return layers.dense(x, units=512, activation=tf.nn.tanh)
+        x = layers.dense(x, units=64, activation=tf.nn.tanh)
+        x = layers.dense(x, units=64, activation=tf.nn.tanh)
+        return x
+
+
+    def output(self):
+        return self.val, self.dist, self.old_dist

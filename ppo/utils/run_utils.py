fd178e3eef8985d92cd5c90c936d87116b4320b6
xff <sailbafan@gmail.com>
2019-02-17 17:18:08 +0800

    added utils

---
+++ ppo/utils/run_utils.py
@@ -0,0 +1,81 @@
+import os
+import os.path as osp
+import string
+import time
+
+# Where experiment outputs are saved by default:
+DEFAULT_DATA_DIR = osp.join(osp.abspath(osp.dirname(osp.dirname(__file__))), 'data')
+
+# Whether to automatically insert a date and time stamp into the names of
+# save directories:
+FORCE_DATESTAMP = False
+
+
+def setup_logger_kwargs(exp_name, env_name, seed=None, data_dir=None, datestamp=False, extra_exp_name=None):
+    """
+    Sets up the output_dir for a logger and returns a dict for logger kwargs.
+
+    If no seed is given and datestamp is false, 
+
+    ::
+
+        output_dir = data_dir/exp_name
+
+    If a seed is given and datestamp is false,
+
+    ::
+
+        output_dir = data_dir/exp_name/exp_name_s[seed]
+
+    If datestamp is true, amend to
+
+    ::
+
+        output_dir = data_dir/YY-MM-DD_exp_name/YY-MM-DD_HH-MM-SS_exp_name_s[seed]
+
+    You can force datestamp=True by setting ``FORCE_DATESTAMP=True`` in 
+    ``spinup/user_config.py``. 
+
+    Args:
+
+        exp_name (string): Name for experiment.
+
+        seed (int): Seed for random number generators used by experiment.
+
+        data_dir (string): Path to folder where results should be saved.
+            Default is the ``DEFAULT_DATA_DIR`` in ``spinup/user_config.py``.
+
+        datestamp (bool): Whether to include a date and timestamp in the
+            name of the save directory.
+
+    Returns:
+
+        logger_kwargs, a dict containing output_dir and exp_name.
+    """
+
+    # Datestamp forcing
+    datestamp = datestamp or FORCE_DATESTAMP
+
+    # Make base path
+    ymd_time = time.strftime("%Y-%m-%d_") if datestamp else ''
+    relpath = ''.join([ymd_time, exp_name])
+    relpath = osp.join(relpath, env_name)
+
+    if seed is not None:
+        # Make a seed-specific subfolder in the experiment directory.
+        if datestamp:
+            hms_time = time.strftime("%Y-%m-%d_%H-%M-%S")
+            subfolder = ''.join([hms_time, '-', exp_name, '_s', str(seed)])
+        else:
+            subfolder = ''.join(['seed', str(seed)])
+        
+        if extra_exp_name:
+            subfolder = osp.join(subfolder, extra_exp_name)
+        relpath = osp.join(relpath, subfolder)
+
+    data_dir = data_dir or DEFAULT_DATA_DIR
+    logger_kwargs = dict(output_dir=osp.join(data_dir, relpath), 
+                         exp_name=exp_name)
+    return logger_kwargs
+
+

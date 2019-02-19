7398d66983675e01e60833075b8ec5139ee57c7c
xff <sailbafan@gmail.com>
2019-02-19 10:57:56 +0800

    set n_env=8 and batch_size=256

--- ppo/run.py
+++ ppo/run.py
@@ -114,10 +114,10 @@ class Runner(object):
         lr = self.lr_schedule.value(self.t)
         clip_ratio = self.clip_ratio_schedule.value(self.t)
         sample_range = np.arange(len(act_buf))
-        for i in range(4):
+        for i in range(3):
             np.random.shuffle(sample_range)
-            for j in range(int(len(act_buf) / 128)):
-                sample_idx = sample_range[128 * j: 128 * (j + 1)]
+            for j in range(int(len(act_buf) / 256)):
+                sample_idx = sample_range[256 * j: 256 * (j + 1)]
                 feed_dict = {
                     self.agent.lr_ph: lr,
                     self.agent.clip_ratio_ph: clip_ratio, 
@@ -191,7 +191,7 @@ if __name__ == "__main__":
     parser.add_argument('--epochs', type=int, default=1000)
     parser.add_argument('--env', type=str, default='BreakoutNoFrameskip-v4')
     parser.add_argument('--seed', '-s', type=int, default=0)
-    parser.add_argument('--n_env', '-n', type=int, default=32)
+    parser.add_argument('--n_env', '-n', type=int, default=8)
     parser.add_argument('--test', action='store_true')
     parser.add_argument('--model', type=int, default=None)
     parser.add_argument('--ext_name', type=str, default='')
